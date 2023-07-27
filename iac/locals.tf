607efafa835f54c898d9cbe9ab3987bb0011029c
Arith Fonseka <arithfonseka@kubrickgroup.com>
2023-07-27 11:14:54 +0100

    Created transformed data storage bucket

--- iac/locals.tf
+++ iac/locals.tf
@@ -1,4 +1,6 @@
 locals {
   raw_storage_bucket_name = format("%s-%s", var.gcp_project_id, "data-bronze-bucket")
+  transformed_storage_bucket_name = format("%s-%s", var.gcp_project_id, "data-silver-bucket")
   google_service_account = format("%s-%s", var.gcp_project_id, "sa")
+
 }
--- iac/main.tf
+++ iac/main.tf
@@ -12,3 +12,16 @@ module "raw_storage_bucket" {
   
 }
 
+
+module "transformed_storage_bucket" {
+    source = "./modules/storage_bucket/"
+    google_storage_bucket_name = local.transformed_storage_bucket_name
+    gcp_project_id = var.gcp_project_id
+    bucket_location = var.gcp_region
+    storage_class = var.storage_class 
+    force_destroy = var.allow_data_deletion_on_tf_destroy
+    uniform_bucket_access = var.uniform_bucket_access 
+    lifecycle_age = var.lifecycle_age
+    action_type = var.action_type
+   
+}
