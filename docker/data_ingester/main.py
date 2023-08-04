eef2dae4483d142758dcdadfd8ddb835aec29138
Arith Fonseka <arithfonseka@kubrickgroup.com>
2023-08-04 09:42:12 +0100

    Written script for data ingestion to gcs

--- docker/data_ingester/main.py
+++ docker/data_ingester/main.py
@@ -1,8 +1,62 @@
 import requests
-import pandas as pd 
+import pandas as pd
+from google.cloud import storage
+from io import StringIO
+from config_variables import var_credentials_location, var_gcs_connector, var_gcs_bronze_bucket
+
+# Initial URL for main web data source
+base_url = r"https://cycling.data.tfl.gov.uk/ActiveTravelCountsProgramme/1-Strategic%20counts%20(CIO)"
+
+# Area List
+area_list = ['Central', 'Inner', 'Outer']
+
+def upload_to_gcs(credentials_location: str, bucket: str, object_name: str, csv_data: pd.DataFrame, file_name:str) -> None:
+    ''' 
+    Function to upload data from local DataFrame to Google Cloud Storage
+    '''
+
+    csv_data.to_csv(file_name)
+
+    storage_client = storage.Client.from_service_account_json(credentials_location)
+    gc_bucket = storage_client.bucket(bucket)
+    blob = gc_bucket.blob(object_name)
+    blob.upload_from_string(file_name, content_type='text/csv')
+    
+    return None
 
 
+def web_to_gcs(year: int, area: str) -> pd.DataFrame:
+    '''
+    Function to take data from online data source and upload to GCS
+    '''
+
+    url = f'{base_url}/{year}-{area}.csv'
+
+    response = requests.get(url)
+    
+    df = pd.read_csv(StringIO(response.text))    
+
+    upload_to_gcs(var_credentials_location, var_gcs_bronze_bucket, f'{year}/{area}', df, file_name)
+    
+    return df
+
+
+def file_name_creator(year: int, area: str) -> str:
+    '''
+    Function create appropriate filename
+    '''
+    file_name = f'{year}_{area}_bikedata.csv'
+
+    return file_name
 
 
 if __name__ == '__main__':
-    print('Successful container run')   
+
+    for year in range(15,23):
+        for area in area_list:
+            file = file_name_creator(year, area)
+            df = web_to_gcs(year, area)
+            upload_to_gcs(var_credentials_location, var_gcs_bronze_bucket, f'{year}/{area}', df, file)
+            print(f'GCS://{year}/{area}')
+
+            
