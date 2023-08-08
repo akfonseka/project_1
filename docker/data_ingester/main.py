eeb414d60601f5a571e024d9cc6f1c12f83b91dc
Arith Fonseka <arithfonseka@kubrickgroup.com>
2023-08-08 15:59:36 +0100

    Updated ingester script

--- docker/data_ingester/main.py
+++ docker/data_ingester/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from google.cloud import storage
 from io import StringIO
 from config_variables import var_gcs_bronze_bucket
+from time import time
 
 # Initial URL for main web data source
 base_url = r"https://cycling.data.tfl.gov.uk/ActiveTravelCountsProgramme/1-Strategic%20counts%20(CIO)"
@@ -10,17 +11,17 @@ base_url = r"https://cycling.data.tfl.gov.uk/ActiveTravelCountsProgramme/1-Strat
 # Area List
 area_list = ['Central', 'Inner', 'Outer']
 
-def upload_to_gcs(credentials_location: str, bucket: str, object_name: str, csv_data: pd.DataFrame, file_name:str) -> None:
+def upload_to_gcs(credentials_location: str, bucket: str, object_name: str, df_data: pd.DataFrame) -> None:
     ''' 
     Function to upload data from local DataFrame to Google Cloud Storage
     '''
 
-    csv_data.to_csv(file_name)
+    # df_data.to_csv(file_name, index=False, header=True)
 
     storage_client = storage.Client.from_service_account_json(credentials_location)
     gc_bucket = storage_client.bucket(bucket)
     blob = gc_bucket.blob(object_name)
-    blob.upload_from_string(file_name, content_type='text/csv')
+    blob.upload_from_string(df_data.to_csv(index=False), content_type='text/csv')
     
     return None
 
@@ -52,10 +53,14 @@ if __name__ == '__main__':
 
 # When run as a script - bikedata between 2015-2022 for all 3 London areas will be downloaded to GCS Bucket
 
-    for year in range(15,23):
+    for year in range(2015,2023):
         for area in area_list:
-            file = file_name_creator(year, area)
+            t_start = time()
+
             df = web_to_df(year, area)
-            upload_to_gcs('./credentials.json', var_gcs_bronze_bucket, f'raw/{year}/{area}', df, file)
-            print(f'GCS: raw/{year}/{area}')
+            upload_to_gcs('./credentials.json', var_gcs_bronze_bucket, f'raw/{year}/{year}_{area}_bikedata.csv', df)
+
+            t_end = time()
+
+            print (f"Downloaded {year}_{area}_bikedata.csv, this took {t_end - t_start:.3f} seconds")
 
