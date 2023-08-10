8a948329678b9a887f266c66cb0448e69c12bd21
Arith Fonseka <arithfonseka@kubrickgroup.com>
2023-08-10 09:43:29 +0100

    Added spark session function spark script

--- docker/spark/main.py
+++ docker/spark/main.py
@@ -1,10 +1,34 @@
 import pyspark
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F 
+from pyspark.sql import types
 from pyspark.conf import SparkConf
 from pyspark.context import SparkContext
-from config_variables import var_credentials_location, var_gcs_connector
 import requests
 import pandas as pd
 from io import StringIO
+import argparse 
+
+# Configure variables as required
+from config_variables import var_credentials_location, var_gcs_connector, var_cluster_temp_bucket
+
+# Area List
+area_list = ['Central', 'Inner', 'Outer']
+
+
+def create_spark_session(app_name: str , temp_bucket: str):
+    conf = SparkConf() \
+        .setAppName(app_name) \
+        .set("temporaryGcsBucket", temp_bucket) 
+
+    
+    spark = SparkSession.builder \
+        .config(conf=conf) \
+        .getOrCreate()
+    
+    return spark 
+
+
+
 
 
